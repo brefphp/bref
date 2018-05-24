dcd952b3e18b7edb20972946b3c86cb59eb34631
Matthieu Napoli <matthieu@mnapoli.fr>
2018-05-24 11:49:11 +0200

    Add Laravel adapter

---
+++ src/Bridge/Laravel/LaravelAdapter.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+namespace Bref\Bridge\Laravel;
+
+use Illuminate\Contracts\Http\Kernel;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+
+/**
+ * Adapter for using the Laravel framework as a HTTP handler.
+ *
+ * @author Matthieu Napoli <matthieu@mnapoli.fr>
+ */
+class LaravelAdapter implements RequestHandlerInterface
+{
+    /**
+     * @var Kernel
+     */
+    private $kernel;
+
+    public function __construct(Kernel $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    public function handle(ServerRequestInterface $request) : ResponseInterface
+    {
+        // Create a Symfony request that will be used by Laravel
+        $httpFoundationFactory = new HttpFoundationFactory;
+        $symfonyRequest = $httpFoundationFactory->createRequest($request);
+
+        // We create Laravel's HTTP request from the Symfony request
+        // We cannot use Symfony's request directly because the Kernel's implementation
+        // expects a `Illuminate\Http\Request` implementation.
+        $laravelRequest = \Illuminate\Http\Request::createFromBase($symfonyRequest);
+
+        /** @var \Illuminate\Http\Response $laravelResponse */
+        $laravelResponse = $this->kernel->handle($laravelRequest);
+        $this->kernel->terminate($laravelRequest, $laravelResponse);
+
+        $psr7Factory = new DiactorosFactory;
+        // The Laravel response extends Symfony so this works fine here
+        $response = $psr7Factory->createResponse($laravelResponse);
+
+        return $response;
+    }
+}
