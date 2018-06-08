6426a9fd5c5e19f26531684cccf29c8b4111159f
Matthieu Napoli <matthieu@mnapoli.fr>
2018-06-08 11:56:46 +0200

    Add a phpinfo command to help debug the PHP configuration on lambdas

--- bref.php
+++ bref.php
@@ -29,6 +29,13 @@ $silly = new Application;
 $silly->command('hello [name]', function (string $name = 'World!', OutputInterface $output) {
     $output->writeln('Hello ' . $name);
 });
+$silly->command('phpinfo', function (OutputInterface $output) {
+    ob_start();
+    phpinfo();
+    $phpinfo = ob_get_contents();
+    ob_clean();
+    $output->write($phpinfo);
+});
 
 $app = new \Bref\Application;
 $app->simpleHandler(function (array $event) {
