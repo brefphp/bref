ab67767d559f85df28719a83e69ab505475c69eb
Matthieu Napoli <matthieu@mnapoli.fr>
2018-06-25 18:21:13 +0200

    Add a Makefile to launch all necessary scripts

---
+++ Makefile
@@ -0,0 +1,3 @@
+# Build all the PHP versions supported by Bref
+build-php:
+	bin/php/build.sh 7.2.5
--- bin/php/build.sh
+++ bin/php/build.sh
@@ -3,30 +3,41 @@
 # This script builds a docker container, compiles PHP for use with AWS Lambda,
 # and copies the final binary to the host and then removes the container.
 #
-# You can specify the PHP Version by setting the branch corresponding to the
-# source from https://github.com/php/php-src
+# You can specify the PHP Version as the first argument.
 
 set -e
 
-# TODO Make a `Makefile` and let us run this script using make
-# The idea is to hide the details of all the tasks behind a single tool.
-# The PHP version could be passed as a command argument instead of the variable below.
+# Set the current directory as this file's directory
+cd "$(dirname "$0")"
 
-PHP_VERSION_GIT_BRANCH=php-7.2.5
+if [[ $# -eq 0 ]] ; then
+    echo 'The PHP version to build must be passed as the first argument'
+    exit 1
+fi
 
-echo "Build PHP Binary from current branch '$PHP_VERSION_GIT_BRANCH' on https://github.com/php/php-src"
+PHP_VERSION=$1
 
-docker build --build-arg PHP_VERSION=$PHP_VERSION_GIT_BRANCH -t php-build .
+echo "Building PHP binary from tag 'php-$PHP_VERSION' on https://github.com/php/php-src"
+
+# Compile PHP
+docker build --build-arg PHP_VERSION=php-$PHP_VERSION -t php-build .
 
 container=$(docker create php-build)
 
-docker -D cp $container:/php-src-$PHP_VERSION_GIT_BRANCH/sapi/cli/php .
+# Fetch the PHP binary in the container
+docker -D cp $container:/php-src-php-$PHP_VERSION/sapi/cli/php .
+
+# Fetch the extensions that were built too
 mkdir -p ext
 docker -D cp $container:/usr/local/lib/php/extensions/no-debug-non-zts-20170718/. ext/
+
 docker rm $container
 
-tar czf $PHP_VERSION_GIT_BRANCH.tar.gz php ext
+# Put all that in an archive
+tar czf php-$PHP_VERSION.tar.gz php ext
 rm php
 rm -rf ext
-aws s3 cp $PHP_VERSION_GIT_BRANCH.tar.gz s3://bref-php/bin/ --acl public-read
-rm $PHP_VERSION_GIT_BRANCH.tar.gz
+
+# Upload the archive to AWS S3
+aws s3 cp php-$PHP_VERSION.tar.gz s3://bref-php/bin/ --acl public-read
+rm php-$PHP_VERSION.tar.gz
