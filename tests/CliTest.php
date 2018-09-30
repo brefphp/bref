e6d4c075102e984afa2567c2d24dfea6f5fa7a85
nealio82 <nealio82@gmail.com>
2018-09-30 13:10:21 +0100

    replace @expectedException annotation with ->expectException

--- tests/CliTest.php
+++ tests/CliTest.php
@@ -4,16 +4,16 @@ declare(strict_types=1);
 namespace Bref\Test;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
 class CliTest extends TestCase
 {
-    /**
-     * @expectedException \Symfony\Component\Process\Exception\ProcessFailedException
-     * @expectedExceptionMessage The files `bref.php` and `serverless.yml` are required to deploy
-     */
     public function test deploying requires mandatory files to exist()
     {
+        $this->expectException(ProcessFailedException::class);
+        $this->expectExceptionMessage('The files `bref.php` and `serverless.yml` are required to deploy');
+
         $process = new Process(__DIR__ . '/../bref deploy', __DIR__ . '/Fixture/EmptyDirectory');
         $process->mustRun();
     }
