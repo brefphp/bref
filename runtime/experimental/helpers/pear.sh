ac6bbb0c9e480764a7cf70afa619bf532aee9726
Bubba <rob@stechstudio.com>
2019-01-08 12:00:16 -0500

    No longer used.

--- runtime/experimental/helpers/pear.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/expect
-spawn /opt/bref/bin/curl -k -o /tmp/go-pear.phar http://pear.php.net/go-pear.phar
-expect eof
-spawn /opt/bref/bin/php /tmp/go-pear.phar
-
-expect "1-11, 'all' or Enter to continue:"
-send "\r"
-expect eof
