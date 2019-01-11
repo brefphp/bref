57e237b9b58b919cfa013bc8cf69a3c8cdd0709b
Matthieu Napoli <matthieu@mnapoli.fr>
2019-01-11 17:06:16 +0100

    Simplify the publishing script by skipping the upload to S3

--- runtime/experimental/helpers/publish.sh
+++ runtime/experimental/helpers/publish.sh
@@ -4,7 +4,6 @@
 
 set -e
 
-BREF_VERSION=0.3
 PHP_VERSION=7.2
 
 if [[ -z "$REGION" ]] ; then
@@ -20,14 +19,22 @@ if [[ -z "$FILE_NAME" ]] ; then
     exit 1
 fi
 
-S3_BUCKET=bref-php-$REGION
-S3_KEY=$BREF_VERSION/$FILE_NAME
-
-# Upload the zipped layer to S3
-aws s3 cp $FILE_NAME s3://$S3_BUCKET/$S3_KEY
-
 # Publish the layer
-LAYER_VERSION=$(aws lambda publish-layer-version --region=$REGION --layer-name $LAYER_NAME --description "PHP $PHP_VERSION" --license-info "MIT" --content S3Bucket=$S3_BUCKET,S3Key=$S3_KEY --compatible-runtimes provided --output text --query Version)
+LAYER_VERSION=$(aws lambda publish-layer-version \
+    --region=$REGION \
+    --layer-name $LAYER_NAME \
+    --description "PHP $PHP_VERSION" \
+    --license-info "MIT" \
+    --zip-file fileb://$FILE_NAME \
+    --compatible-runtimes provided \
+    --output text \
+    --query Version)
 
 # Add layer permissions
-aws lambda add-layer-version-permission --region=$REGION --layer-name $LAYER_NAME --version-number $LAYER_VERSION --statement-id=public --action lambda:GetLayerVersion --principal '*'
+aws lambda add-layer-version-permission \
+    --region=$REGION \
+    --layer-name $LAYER_NAME \
+    --version-number $LAYER_VERSION \
+    --statement-id=public \
+    --action lambda:GetLayerVersion \
+    --principal '*'
