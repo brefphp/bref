d1c3fc7c281ca26e0e81f1b93e3d989b9152f1cc
Matthieu Napoli <matthieu@mnapoli.fr>
2019-01-19 15:38:48 +0100

    Simplify the demo to make it like the examples in the documentation

--- demo/function.php
+++ demo/function.php
@@ -5,7 +5,5 @@ require __DIR__ . '/../vendor/autoload.php';
 ini_set('display_errors', '1');
 
 lambda(function (array $event) {
-    return [
-        'hello' => $event['name'] ?? 'world',
-    ];
+    return 'Hello ' . ($event['name'] ?? 'world');
 });
