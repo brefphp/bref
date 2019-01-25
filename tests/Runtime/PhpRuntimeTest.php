eec3c3b55c991a8aaf868041ec788dd85074741d
Matthieu Napoli <matthieu@mnapoli.fr>
2019-01-25 13:51:27 +0100

    Add tests for the PHP runtime using `sam`

---
+++ tests/Runtime/PhpRuntimeTest.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace Bref\Test\Runtime;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Process\Process;
+
+class PhpRuntimeTest extends TestCase
+{
+    public function test simple invocation()
+    {
+        $process = new Process(['sam', 'local', 'invoke', 'PhpFunction', '--no-event', '--region', 'us-east-1']);
+        $process->mustRun();
+        $result = json_decode(trim($process->getOutput()), true);
+        self::assertSame('Hello world', $result, $process->getErrorOutput());
+    }
+}
