a4311d927fef226531f481bfa51e91369174bc68
Matthieu Napoli <matthieu@mnapoli.fr>
2019-01-26 14:29:12 +0100

    Test the extensions enabled by default in the PHP runtime

--- tests/Runtime/PhpRuntime/function.php
+++ tests/Runtime/PhpRuntime/function.php
@@ -5,5 +5,9 @@ require __DIR__ . '/../../../vendor/autoload.php';
 ini_set('display_errors', '1');
 
 lambda(function (array $event) {
+    if ($event['extensions'] ?? false) {
+        return get_loaded_extensions();
+    }
+
     return 'Hello ' . ($event['name'] ?? 'world');
 });
