73d6c0ddb9fc2e6b5aaa8237e313db522b19cb7e
Matthieu Napoli <matthieu@mnapoli.fr>
2019-01-26 13:03:22 +0100

    Fix test

--- tests/Runtime/PhpRuntimeTest.php
+++ tests/Runtime/PhpRuntimeTest.php
@@ -10,6 +10,7 @@ class PhpRuntimeTest extends TestCase
     public function test simple invocation()
     {
         $process = new Process(['sam', 'local', 'invoke', 'PhpFunction', '--no-event', '--region', 'us-east-1']);
+        $process->setWorkingDirectory(__DIR__);
         $process->mustRun();
         $result = json_decode(trim($process->getOutput()), true);
         self::assertSame('Hello world', $result, $process->getErrorOutput());
