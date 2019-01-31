f71b4ecbbdcdd1f2541d0ca25ceb8dc40aca2006
Bubba <rob@stechstudio.com>
2019-01-31 14:07:47 -0500

    Bref service provider for laravel

---
+++ src/Bridge/Laravel/ServiceProvider.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Bref\Bridge\Laravel;
+
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
+
+class ServiceProvider extends IlluminateServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // if we are running in lambda, lets shuffle some things around.
+        if (runningInLambda()) {
+            $this->setupStorage();
+            $this->setupSessionDriver();
+            $this->setupLogStack();
+        }
+        // Allow artisan
+        $this->publishes([
+            __DIR__ . '/config/cloudformation.yaml' => base_path('template.yaml'),
+        ]);
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Since the lambda filesystem is readonly except for
+     * `/tmp` we need to customize the storage area.
+     */
+    public function setupStorage(): void
+    {
+        $storagePath = '/tmp/storage';
+
+        $storagePaths = [
+            "/app/public",
+            "/framework/cache/data",
+            "/framework/sessions",
+            "/framework/testing",
+            "/framework/views",
+            "/logs"
+        ];
+
+        foreach ($storagePaths as $path) {
+            mkdir($storagePath . $path, 0777, true);
+        }
+        $this->app->useStoragePath($storagePath);
+    }
+
+    /**
+     * Lambda cannot persist sessions to disk.
+     */
+    public function setupSessionDriver(): void
+    {
+        // if you try to we will override
+        // you and save you from yourself.
+        if (env(SESSION_DRIVER) == 'file') {
+            # If you need sessions, store them
+            # in redis, a database, or cookies
+            # anything that scales horizontally
+            putenv("SESSION_DRIVER=array");
+            Config::set('session.driver', 'array');
+        }
+    }
+
+    /**
+     * At this point, the default single and daily logs will
+     * log to `storage_path('logs/laravel.log')` and we have that
+     * aimed at /tmp already. But that doesn't do anyone any good.
+     * We expect the logs to all go to STDERR so that lambda just
+     * automatically logs them to CloudWatch.
+     */
+    public function setupLogStack(): void
+    {
+        // So here is what I am going to do. I will check t
+        // o see if you want me to leave your logging alone.
+        if (getenv('LEAVE_MY_LOGS_ALONE') !== false) {
+            // and if you didn't tell me to leave your settings alone
+            // and if you aren't already using the stderr channel
+            if (Config::get('logging.default') !== 'stderr') {
+                # I will inject stderr into whatever you are doing.
+                if (Config::get('logging.default') == 'stack') {
+                    # Good, you are already using the stack.
+                    $channels = Config::get('logging.channels.stack.channels');
+                    if (!in_array('stderr', $channels)) {
+                        $channels[] = 'stderr';
+                    }
+                } else {
+                    # Just gonna setup a stack log channel for you here.
+                    $channels = ['stderr', Config::get('logging.default')];
+                }
+                Config::Set('logging.channels.stack.channels', $channels);
+                Config::set('logging.default', 'stack');
+            }
+        }
+    }
+}
