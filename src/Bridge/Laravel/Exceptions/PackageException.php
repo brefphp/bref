bbc426738e9dcca92b9f244218c1ce9c9c42bfdd
Bubba <rob@stechstudio.com>
2019-01-31 17:53:02 -0500

    Except this

---
+++ src/Bridge/Laravel/Exceptions/PackageException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bubba
+ * Date: 2019-01-31
+ * Time: 16:34
+ */
+
+namespace Bref\Bridge\Laravel\Exceptions;
+
+
+class PackageException extends \RuntimeException
+{
+}
