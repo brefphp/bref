9bf80e4a03331157719ac58a43d869df48946d31
Bubba <rob@stechstudio.com>
2019-02-03 12:04:43 -0500

    Latest default configuration

--- src/Bridge/Laravel/config/bref.php
+++ src/Bridge/Laravel/config/bref.php
@@ -1,5 +1,28 @@
 <?php
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Bref Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your Lambda. This value is used when the
+    | framework needs to generate the lambda function names.
+    |
+    */
+    'name' => env('BREF_NAME', env('APP_NAME')),
+
+    'website_name' => env('BREF_NAME', env('APP_NAME')) . '-website-' . env('APP_ENV'),
+    'artisan_name' => env('BREF_NAME', env('APP_NAME')) . '-artisan-' . env('APP_ENV'),
+
+    /*
+    |--------------------------------------------------------------------------
+    |Packaging
+    |--------------------------------------------------------------------------
+    |
+    | This array configures the files that should be ignored when packaging
+    | your application, as well as identifying executable files.
+    |
+    */
     'packaging' => [
         'ignore' => [
             // Directories & Fully Qualified Paths
@@ -8,7 +31,7 @@ return [
             base_path('storage'),
             base_path('.idea'),
             base_path('.git'),
-            // Files Names
+            // File Names
             '.gitignore',
             '.env',
             '.env.example',
@@ -28,9 +51,9 @@ return [
             'server.php',
             'template.yaml'
         ],
+        // Any executables should be here.
         'executables' => [
             'artisan'
         ]
-
     ]
 ];
