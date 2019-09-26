b1060c903e6d5a4d101113911dd48da5f13b056a
Matthieu Napoli <matthieu@mnapoli.fr>
2019-09-26 13:45:03 +0200

    Add cases to manually test #457

--- demo/http.php
+++ demo/http.php
@@ -6,4 +6,24 @@ if (isset($_GET['sleep'])) {
     sleep(10);
 }
 
+if (isset($_GET['img'])) {
+    $fp = fopen(__DIR__ . '/../website/template/img/logo-small.png', 'rb');
+    header('Content-Type: image/png');
+    header('Content-Length: ' . filesize(__DIR__ . '/logo.png'));
+    fpassthru($fp);
+    exit(0);
+}
+
+if (isset($_GET['json'])) {
+    header('Content-Type: application/json');
+    echo json_encode(['Hello' => '🌍']);
+    exit(0);
+}
+
+if (isset($_GET['weird'])) {
+    header('Content-Type: foo/bar');
+    echo 'Hello 🌍';
+    exit(0);
+}
+
 echo 'Hello world!';
