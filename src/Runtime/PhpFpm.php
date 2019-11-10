15ae82837be3e13c7e90ad8413e2313c70e5e05b
pl4yradam <adam@pl4yr.com>
2019-11-10 21:00:18 +0000

    Updated FPM php to cater for api gateways authorisers
    This request context is needed if a user is using the authentication type of IAM_ROLE, when a user auths through Api Gateway it will then authorise the user and pass their information (securely as request context). At the moment the file misses this so a php function cant use the authorised details correctly.

--- src/Runtime/PhpFpm.php
+++ src/Runtime/PhpFpm.php
@@ -209,7 +209,8 @@ final class PhpFpm
         $request->setServerPort((int) ($headers['x-forwarded-port'][0] ?? 80));
         $request->setCustomVar('PATH_INFO', $event['path'] ?? '/');
         $request->setCustomVar('QUERY_STRING', $queryString);
-
+        $request->setCustomVar('REQUEST_CONTEXT', json_encode($event['requestContext']));
+        
         // See https://stackoverflow.com/a/5519834/245552
         if (! empty($requestBody) && $method !== 'TRACE' && ! isset($headers['content-type'])) {
             $headers['content-type'] = ['application/x-www-form-urlencoded'];
