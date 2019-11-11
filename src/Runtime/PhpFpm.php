06880dfcc05c6f92b28345641091700507ffec4c
Adam Kernig <adam.kernig@kernigservices.ltd>
2019-11-11 21:28:51 +0000

    test

--- src/Runtime/PhpFpm.php
+++ src/Runtime/PhpFpm.php
@@ -214,7 +214,7 @@ final class PhpFpm
 //            foreach ($event['requestContext'] as $key => $value) {
 //                $request->setCustomVar('REQUEST_CONTEXT_' . strtoupper($key), $value);
 //            }
-            $request->setCustomVar('REQUEST_CONTEXT', []);
+            $request->addCustomVars(['test' => 'test']);
         }
         // See https://stackoverflow.com/a/5519834/245552
         if (! empty($requestBody) && $method !== 'TRACE' && ! isset($headers['content-type'])) {
