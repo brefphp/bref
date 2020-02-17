968ab85304762ccb6b8a608d4b1d8a1d1fd52e4f
Jason McCallister <jason@craftcms.com>
2020-02-17 09:16:31 -0500

    Add tests for dynamodb record
    Signed-off-by: Jason McCallister <jason@craftcms.com>

--- src/Event/DynamoDb/DynamoDbRecord.php
+++ src/Event/DynamoDb/DynamoDbRecord.php
@@ -14,7 +14,7 @@ final class DynamoDbRecord
     public function __construct($record)
     {
         if (!is_array($record) || !isset($record['eventSource']) || $record['eventSource'] !== 'aws:dynamodb') {
-            throw new \InvalidArgumentException;
+            throw new \InvalidArgumentException('Event source must come from DynamoDB');
         }
 
         $this->record = $record;
@@ -23,5 +23,30 @@ final class DynamoDbRecord
     public function getKeys()
     {
         return $this->record['dynamodb']['Keys'];
+    }
+
+    public function getNewImage()
+    {
+        return $this->record['dynamodb']['NewImage'];
+    }
+
+    public function getOldImage()
+    {
+        return $this->record['dynamodb']['OldImage'] ?? null;
+    }
+
+    public function getSequenceNumber()
+    {
+        return $this->record['dynamodb']['SequenceNumber'];
+    }
+
+    public function getSizeBytes()
+    {
+        return $this->record['dynamodb']['SizeBytes'];
+    }
+
+    public function getStreamViewType()
+    {
+        return $this->record['dynamodb']['StreamViewType'];
     }
 }
--- tests/Event/DynamoDb/DynamoDbEventTest.php
+++ tests/Event/DynamoDb/DynamoDbEventTest.php
@@ -11,12 +11,23 @@ class DynamoDbEventTest extends TestCase
     {
         // Arrange
         $event = json_decode(file_get_contents(__DIR__ . '/dynamodb.json'), true);
+        $keys = ['Id' => ['N' => '101']];
+        $newImage = [
+            'Message' => ['S' => 'New item!'],
+            'Id' => ['N' => '101'],
+        ];
 
         // Act
         $event = new DynamoDbEvent($event);
-        $records = $event->getRecords()[0];
+        $newRecord = $event->getRecords()[0];
+        $updatedRecord = $event->getRecords()[1];
 
         // Assert
-        $this->assertSame(['Id' => ['N' => '101']], $records->getKeys());
+        $this->assertSame($keys, $newRecord->getKeys());
+        $this->assertSame($newImage, $newRecord->getNewImage());
+        $this->assertNull($newRecord->getOldImage());
+        $this->assertSame('111', $newRecord->getSequenceNumber());
+        $this->assertSame(26, $newRecord->getSizeBytes());
+        $this->assertSame('NEW_AND_OLD_IMAGES', $newRecord->getStreamViewType());
     }
 }
