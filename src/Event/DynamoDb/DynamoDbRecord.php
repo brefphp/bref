8ebc54feb97d79eb30b094afac5f1153f971fc5c
Patrice MAYET <patrice.mayet@gmail.com>
2020-03-31 13:36:40 +0200

    Add DynamoDB Stream typed events

---
+++ src/Event/DynamoDb/DynamoDbRecord.php
@@ -0,0 +1,81 @@
+<?php declare(strict_types=1);
+
+namespace Bref\Event\DynamoDb;
+
+/**
+ * @see https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_StreamSpecification.html
+ */
+
+final class DynamoDbRecord
+{
+    /** @var array */
+    private $record;
+
+    /**
+     * @param mixed $record
+     */
+    public function __construct($record)
+    {
+        if (! is_array($record) || ! isset($record['eventSource']) || $record['eventSource'] !== 'aws:dynamodb') {
+            throw new \InvalidArgumentException('Event source must come from DynamoDB');
+        }
+
+        $this->record = $record;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEventName(): string
+    {
+        return $this->record['eventName'];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getKeys()
+    {
+        return $this->record['dynamodb']['Keys'];
+    }
+
+    /**
+     * @return mixed|null
+     */
+    public function getNewImage()
+    {
+        return $this->record['dynamodb']['NewImage'] ?? null;
+    }
+
+    /**
+     * @return mixed|null
+     */
+    public function getOldImage()
+    {
+        return $this->record['dynamodb']['OldImage'] ?? null;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSequenceNumber(): string
+    {
+        return $this->record['dynamodb']['SequenceNumber'];
+    }
+
+    /**
+     * @return int
+     */
+    public function getSizeBytes(): int
+    {
+        return $this->record['dynamodb']['SizeBytes'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getStreamViewType(): string
+    {
+        return $this->record['dynamodb']['StreamViewType'];
+    }
+}
