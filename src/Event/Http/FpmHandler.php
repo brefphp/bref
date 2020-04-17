aef9dab826528712acff0971de093eae2eafc20e
Edd Mann <the@eddmann.com>
2020-04-17 22:56:19 +0100

    Add accompanying Lambda context documentation

--- src/Event/Http/FpmHandler.php
+++ src/Event/Http/FpmHandler.php
@@ -181,6 +181,8 @@ final class FpmHandler extends HttpHandler
         $request->setCustomVar('QUERY_STRING', $event->getQueryString());
         $request->setCustomVar('LAMBDA_INVOCATION_CONTEXT', json_encode($context));
         $request->setCustomVar('LAMBDA_REQUEST_CONTEXT', json_encode($event->getRequestContext()));
+
+        /** @deprecated The LAMBDA_CONTEXT has been renamed to LAMBDA_REQUEST_CONTEXT for clarity */
         $request->setCustomVar('LAMBDA_CONTEXT', json_encode($event->getRequestContext()));
 
         $contentType = $event->getContentType();
