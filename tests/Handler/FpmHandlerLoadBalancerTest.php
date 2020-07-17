9e898d08270057be7463ed351f45a424760177d2
Deleu <deleugyn@gmail.com>
2020-07-17 14:50:11 +0200

    Add tests with multiple values with numeric keys

--- tests/Handler/FpmHandlerLoadBalancerTest.php
+++ tests/Handler/FpmHandlerLoadBalancerTest.php
@@ -17,14 +17,14 @@ final class FpmHandlerLoadBalancerTest extends TestCase
     {
         parent::setUp();
 
-//        ob_start();
+        ob_start();
         $this->fakeContext = new Context('abc', time(), 'abc', 'abc');
     }
 
     public function tearDown(): void
     {
         $this->fpm->stop();
-//        ob_end_clean();
+        ob_end_clean();
     }
 
     /**
@@ -89,7 +89,48 @@ final class FpmHandlerLoadBalancerTest extends TestCase
 
         self::assertSame('segment_a', $body['$_GET']['conditions']['survey'][0]['field']);
         self::assertSame('=', $body['$_GET']['conditions']['survey'][0]['operator']);
-        self::assertSame('23', $body['$_GET']['conditions']['survey'][0]['values'][0]);
+        self::assertSame('64', $body['$_GET']['conditions']['survey'][0]['values'][0]);
+        self::assertSame('40', $body['$_GET']['conditions']['survey'][0]['values'][1]);
+        self::assertSame('23', $body['$_GET']['conditions']['survey'][0]['values'][2]);
+        self::assertSame('2020-06-18', $body['$_GET']['period']['survey']['start']);
+        self::assertSame('2020-07-17', $body['$_GET']['period']['survey']['end']);
+        self::assertSame('date_email_sent', $body['$_GET']['period']['survey']['field']);
+    }
+
+    public function test alb query strings encoded with multi value enabled()
+    {
+        $loadBalancerEvent = file_get_contents(__DIR__ . '/PhpFpm/alb-multi-value-query-string-encoded.json');
+
+        $response = $this->get('request.php', json_decode($loadBalancerEvent, true));
+
+        $body = json_decode($response['body'], true);
+
+        self::assertSame('101', $body['$_GET']['filter']['destination'][0]);
+        self::assertSame('103', $body['$_GET']['filter']['destination'][1]);
+        self::assertSame('106', $body['$_GET']['filter']['destination'][2]);
+        self::assertSame('month', $body['$_GET']['date_increment']);
+    }
+
+    public function test alb query strings with multi value enabled includes numeric keys()
+    {
+        $loadBalancerEvent = file_get_contents(__DIR__ . '/PhpFpm/alb-multi-value-query-string-with-numeric-keys.json');
+
+        $response = $this->get('request.php', json_decode($loadBalancerEvent, true));
+
+        $body = json_decode($response['body'], true);
+
+        self::assertSame('segment_a', $body['$_GET']['conditions']['survey'][0]['field']);
+        self::assertSame('=', $body['$_GET']['conditions']['survey'][0]['operator']);
+        self::assertSame('64', $body['$_GET']['conditions']['survey'][0]['values'][0]);
+        self::assertSame('40', $body['$_GET']['conditions']['survey'][0]['values'][1]);
+        self::assertSame('23', $body['$_GET']['conditions']['survey'][0]['values'][2]);
+
+        self::assertSame('segment_c', $body['$_GET']['conditions']['survey'][1]['field']);
+        self::assertSame('=', $body['$_GET']['conditions']['survey'][1]['operator']);
+        self::assertSame('10', $body['$_GET']['conditions']['survey'][1]['values'][0]);
+        self::assertSame('9', $body['$_GET']['conditions']['survey'][1]['values'][1]);
+        self::assertSame('6', $body['$_GET']['conditions']['survey'][1]['values'][2]);
+
         self::assertSame('2020-06-18', $body['$_GET']['period']['survey']['start']);
         self::assertSame('2020-07-17', $body['$_GET']['period']['survey']['end']);
         self::assertSame('date_email_sent', $body['$_GET']['period']['survey']['field']);
