e1f7ac40e0b3f881dd25e2def28ac65369a03508
Deleu <deleugyn@gmail.com>
2020-07-17 12:05:40 +0200

    parameter decoding for multi value disabled ALB

--- tests/Handler/FpmHandlerLoadBalancerTest.php
+++ tests/Handler/FpmHandlerLoadBalancerTest.php
@@ -17,28 +17,82 @@ final class FpmHandlerLoadBalancerTest extends TestCase
     {
         parent::setUp();
 
-        ob_start();
+//        ob_start();
         $this->fakeContext = new Context('abc', time(), 'abc', 'abc');
     }
 
     public function tearDown(): void
     {
         $this->fpm->stop();
-        ob_end_clean();
+//        ob_end_clean();
     }
 
-    public function test request with multivalues query string have basic support()
+    /**
+     * This one tests query strings unencoded coming from ALB. Only the last value sent is kept by ALB.
+     * Multi values parameters are lost. The fixture contains only the actual data that ALB ended up
+     * sending to Lambda. conditions[survey][0][values][] was suppose to have multiple values, but
+     * only 23 was kept.
+     */
+    public function test alb query strings()
     {
-        $loadBalancerEvent = file_get_contents(__DIR__ . '/PhpFpm/alb-query-string-multivalue.json');
+        $loadBalancerEvent = file_get_contents(__DIR__ . '/PhpFpm/alb-query-string.json');
 
         $response = $this->get('request.php', json_decode($loadBalancerEvent, true));
 
         $body = json_decode($response['body'], true);
 
-        self::assertSame($body['$_GET']['date']['from'], '2019-07-18');
-        self::assertSame($body['$_GET']['date']['to'], '2020-07-16');
-        self::assertSame($body['$_GET']['date']['type'], 'date_email_sent');
-        self::assertSame($body['$_GET']['filter']['destination'][0], '101');
+        self::assertSame('segment_a', $body['$_GET']['conditions']['survey'][0]['field']);
+        self::assertSame('=', $body['$_GET']['conditions']['survey'][0]['operator']);
+        self::assertSame('23', $body['$_GET']['conditions']['survey'][0]['values'][0]);
+        self::assertSame('2020-06-18', $body['$_GET']['period']['survey']['start']);
+        self::assertSame('2020-07-17', $body['$_GET']['period']['survey']['end']);
+        self::assertSame('date_email_sent', $body['$_GET']['period']['survey']['field']);
+    }
+
+    /**
+     * Same as `test alb query strings`. However, this time the query strings are URL encoded.
+     * Multi value is not enabled.
+     */
+    public function test alb query strings encoded()
+    {
+        $loadBalancerEvent = file_get_contents(__DIR__ . '/PhpFpm/alb-query-string-encoded.json');
+
+        $response = $this->get('request.php', json_decode($loadBalancerEvent, true));
+
+        $body = json_decode($response['body'], true);
+
+        self::assertSame('2019-07-19', $body['$_GET']['date']['from']);
+        self::assertSame('2020-07-17', $body['$_GET']['date']['to']);
+        self::assertSame('date_email_sent', $body['$_GET']['date']['type']);
+        self::assertSame('101', $body['$_GET']['filter']['destination'][0]);
+    }
+
+    public function test alb query strings mixed encoding()
+    {
+        $loadBalancerEvent = file_get_contents(__DIR__ . '/PhpFpm/alb-query-string-mixed-encoding.json');
+
+        $response = $this->get('request.php', json_decode($loadBalancerEvent, true));
+
+        $body = json_decode($response['body'], true);
+
+        self::assertSame('abc123!@#%', $body['$_GET']['encoded_value'][0]);
+        self::assertSame('106', $body['$_GET']['filter']['destination'][0]);
+    }
+
+    public function test alb query strings with multi value enabled()
+    {
+        $loadBalancerEvent = file_get_contents(__DIR__ . '/PhpFpm/alb-multi-value-query-string.json');
+
+        $response = $this->get('request.php', json_decode($loadBalancerEvent, true));
+
+        $body = json_decode($response['body'], true);
+
+        self::assertSame('segment_a', $body['$_GET']['conditions']['survey'][0]['field']);
+        self::assertSame('=', $body['$_GET']['conditions']['survey'][0]['operator']);
+        self::assertSame('23', $body['$_GET']['conditions']['survey'][0]['values'][0]);
+        self::assertSame('2020-06-18', $body['$_GET']['period']['survey']['start']);
+        self::assertSame('2020-07-17', $body['$_GET']['period']['survey']['end']);
+        self::assertSame('date_email_sent', $body['$_GET']['period']['survey']['field']);
     }
 
     private function get(string $file, ?array $event = null): array
