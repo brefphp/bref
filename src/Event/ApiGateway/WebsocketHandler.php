0a4e991f1819d1b32abe92edd7e3d9088ce667c0
Dannie Hansen <danniehansenweb@gmail.com>
2020-10-11 15:01:14 +0200

    Made WebsocketHandler accept WebsocketResponse|int as return type

--- src/Event/ApiGateway/WebsocketHandler.php
+++ src/Event/ApiGateway/WebsocketHandler.php
@@ -4,17 +4,31 @@ namespace Bref\Event\ApiGateway;
 
 use Bref\Context\Context;
 use Bref\Event\Handler;
+use RuntimeException;
 
 /**
  * Handles ApiGateway events.
  */
 abstract class WebsocketHandler implements Handler
 {
-    abstract public function handleWebsocket(WebsocketEvent $event, Context $context): WebsocketResponse;
+    /**
+     * @return WebsocketResponse|int
+     */
+    abstract public function handleWebsocket(WebsocketEvent $event, Context $context);
 
     /** {@inheritDoc} */
     public function handle($event, Context $context)
     {
-        return $this->handleWebsocket(new WebsocketEvent($event), $context)->toApiGatewayFormat();
+        $response = $this->handleWebsocket(new WebsocketEvent($event), $context);
+
+        if (is_int($response)) {
+            $response = new WebsocketResponse($response);
+        }
+
+        if (! ($response instanceof WebsocketResponse)) {
+            throw new RuntimeException('handleWebsocket response has to be of type: WebsocketResponse|int');
+        }
+
+        return $response->toApiGatewayFormat();
     }
 }
--- tests/Event/ApiGateway/WebsocketEventTest.php
+++ tests/Event/ApiGateway/WebsocketEventTest.php
@@ -3,7 +3,6 @@
 namespace Bref\Test\Event\DynamoDb;
 
 use Bref\Event\ApiGateway\WebsocketEvent;
-use Bref\Event\ApiGateway\WebsocketResponse;
 use PHPUnit\Framework\TestCase;
 
 class WebsocketEventTest extends TestCase
@@ -48,25 +47,5 @@ class WebsocketEventTest extends TestCase
         $this->assertSame('$default', $event->getRouteKey());
         $this->assertSame('dev', $event->getStage());
         $this->assertSame('Hello Server!', $event->getBody());
-    }
-
-    public function test_response_default()
-    {
-        $response = new WebsocketResponse;
-        $apiGatewayResponse = $response->toApiGatewayFormat();
-
-        $this->assertIsArray($apiGatewayResponse);
-        $this->assertArrayHasKey('statusCode', $apiGatewayResponse);
-        $this->assertSame(200, $apiGatewayResponse['statusCode']);
-    }
-
-    public function test_response_internal_server_error()
-    {
-        $response = new WebsocketResponse(500);
-        $apiGatewayResponse = $response->toApiGatewayFormat();
-
-        $this->assertIsArray($apiGatewayResponse);
-        $this->assertArrayHasKey('statusCode', $apiGatewayResponse);
-        $this->assertSame(500, $apiGatewayResponse['statusCode']);
     }
 }
