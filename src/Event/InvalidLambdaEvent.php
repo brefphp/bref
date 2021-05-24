ed7d6283010c6f8f882c31101845c09a40d86ed1
Deleu <deleugyn@gmail.com>
2021-05-24 12:03:09 +0200

    Improve error message when invoking Http Event with invalid payload

--- src/Event/InvalidLambdaEvent.php
+++ src/Event/InvalidLambdaEvent.php
@@ -11,7 +11,12 @@ final class InvalidLambdaEvent extends Exception
      */
     public function __construct(string $expectedEventType, $event)
     {
-        $eventData = print_r($event, true);
+        if (! $event) {
+            $eventData = 'null';
+        } else {
+            $eventData = print_r($event, true);
+        }
+
         parent::__construct("This handler expected to be invoked with a $expectedEventType event. Instead, the handler was invoked with invalid event data: $eventData");
     }
 }
