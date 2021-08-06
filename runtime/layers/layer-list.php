874c2901fd70f65db50c5fff4f475e876c8f7529
Deleu <deleugyn@gmail.com>
2021-08-06 18:17:40 +0200

    Testing assume-role

--- runtime/layers/layer-list.php
+++ runtime/layers/layer-list.php
@@ -7,6 +7,7 @@
  */
 
 use AsyncAws\Lambda\LambdaClient;
+use AsyncAws\Core\Sts\StsClient;
 use AsyncAws\Lambda\ValueObject\LayerVersionsListItem;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
@@ -21,35 +22,64 @@ const LAYER_NAMES = [
     'console',
 ];
 
-$regions = json_decode(file_get_contents(__DIR__ . '/regions.json'), true);
+//$regions = json_decode(file_get_contents(__DIR__ . '/regions.json'), true);
 
 $export = [];
-foreach ($regions as $region) {
-    $layers = listLayers($region);
+//foreach ($regions as $region) {
+    $region = 'eu-west-1';
+
+    $client = lambdaClient($region);
+
+    $layers = listLayers($client, $region);
     foreach (LAYER_NAMES as $layerName) {
         $export[$layerName][$region] = $layers[$layerName];
     }
     echo "$region\n";
-}
+//}
 file_put_contents(__DIR__ . '/../../layers.json', json_encode($export, JSON_PRETTY_PRINT));
 echo "Done\n";
 
 
-function listLayers(string $selectedRegion): array
+function lambdaClient(string $region): LambdaClient
 {
+    if (getenv('AWS_STS_REGIONAL_ENDPOINTS') === 'regional') {
+        $stsClient = new StsClient([
+            'region' => $region,
+            'version' => '2011-06-15'
+        ]);
 
-    $lambda = new LambdaClient([
-        'region' => $selectedRegion,
-    ]);
+        $credentials = $stsClient->AssumeRole([
+            'RoleArn' => 'arn:aws:iam::179453031647:role/bref-layer-publisher',
+            'RoleSessionName' => 'bref-layer-builder',
+        ]);
 
-    // Run the API calls in parallel (thanks to async)
-    $results = [];
-    foreach (LAYER_NAMES as $layerName) {
-        $results[$layerName] = $lambda->listLayerVersions([
-            'LayerName' => sprintf('arn:aws:lambda:%s:209497400698:layer:%s', $selectedRegion, $layerName),
-            'MaxItems' => 1,
+        return new LambdaClient([
+            'region' => $region,
+            'credentials' => [
+                'key'    => $credentials['Credentials']['AccessKeyId'],
+                'secret' => $credentials['Credentials']['SecretAccessKey'],
+                'token'  => $credentials['Credentials']['SessionToken']
+            ],
+        ]);
+    } else {
+        return new LambdaClient([
+            'region' => $region,
         ]);
     }
+}
+
+function listLayers(LambdaClient $lambda, string $selectedRegion): array
+{
+    // Run the API calls in parallel (thanks to async)
+    $results = [];
+//    foreach (LAYER_NAMES as $layerName) {
+        $layerName = 'GoogleSheetLayers';
+
+        $results[$layerName] = $lambda->listLayerVersions([
+            'LayerName' => sprintf('arn:aws:lambda:%s:179453031647:layer:%s', $selectedRegion, $layerName),
+            'MaxItems' => 1,
+        ]);
+//    }
 
     $layers = [];
     foreach ($results as $layerName => $result) {
