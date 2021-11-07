3040cb3797dcdfbccf1bef897e2a5cfb664ae8d0
Deleu <deleugyn@gmail.com>
2021-11-07 13:36:00 +0100

    Test extensions that come enabled by default

--- runtime/tests/unit/test_extensions.php
+++ runtime/tests/unit/test_extensions.php
@@ -3,6 +3,7 @@
 $provider = [
     'bcmath' => bcadd('4', '5') === '9',
     'ctype' => ctype_digit('4'),
+    'date' => class_exists(\DateTime::class),
     'dom' => class_exists(\DOMDocument::class),
     'exif' => function_exists('exif_imagetype'),
     'fileinfo' => function_exists('finfo_file'),
@@ -12,8 +13,12 @@ $provider = [
     'mbstring' => mb_strlen('12characters') === 12,
     'mysqli' => function_exists('mysqli_connect'),
     'opcache' => ini_get('opcache.enable') == 1 && ini_get('opcache.enable_cli') == 1,
+    'openssl' => strlen(openssl_random_pseudo_bytes(1)) === 1,
     'pdo' => class_exists(\PDO::class),
     'pdo_mysql' => extension_loaded('pdo_mysql'),
+    'readline' => READLINE_LIB === 'libedit',
+    'reflection' => class_exists(\ReflectionClass::class),
+    'session' => session_status() === PHP_SESSION_NONE,
     'simplexml' => class_exists(\SimpleXMLElement::class),
     'sodium' => defined('PASSWORD_ARGON2I'),
 ];
