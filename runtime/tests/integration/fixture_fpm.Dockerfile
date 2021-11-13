803c2a2ee80b11fecc5c7648393aeb93f932b2b6
Deleu <deleugyn@gmail.com>
2021-11-13 19:58:35 +0100

    Preparing rebase

--- runtime/tests/integration/fixture_fpm.Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM alpine:3.14
-
-RUN apk add composer
-
-ENTRYPOINT /usr/bin/tail
-
-CMD ['-f', '/dev/null']
--- runtime/tests/integration/fixture_function.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-return function ($event, \Bref\Context\Context $context) {
-    return [
-        'event' => $event,
-        'server' => $_SERVER,
-        'memory_limit' => ini_get('memory_limit'),
-    ];
-};
