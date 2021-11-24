6865f2b5ae156cdb453a199a64ff0b5b2f0052bc
Thibault RICHARD <thibault@widop.com>
2021-11-24 13:49:14 +0100

    Support SQS partial batch failure

--- src/Event/Sqs/SqsHandler.php
+++ src/Event/Sqs/SqsHandler.php
@@ -10,11 +10,36 @@ use Bref\Event\Handler;
  */
 abstract class SqsHandler implements Handler
 {
+    /** @var SqsRecord[] */
+    private $failedRecords = [];
+
     abstract public function handleSqs(SqsEvent $event, Context $context): void;
 
     /** {@inheritDoc} */
-    public function handle($event, Context $context): void
+    public function handle($event, Context $context)
     {
+        // Reset the failed records to clear the internal state when using BREF_LOOP_MAX
+        $this->failedRecords = [];
+
         $this->handleSqs(new SqsEvent($event), $context);
+
+        if (count($this->failedRecords) === 0) {
+            return;
+        }
+
+        $failures = array_map(
+            function (SqsRecord $record) {
+                return ['itemIdentifier' => $record->getMessageId()];
+            },
+            $this->failedRecords
+        );
+
+        return [
+            'batchItemFailures' => $failures
+        ];
+    }
+
+    final protected function markAsFailed(SqsRecord $record) {
+        $this->failedRecords[] = $record;
     }
 }
