da42471665ed65e8169d02f0f8e766e25ce5a63d
Deleu <deleugyn@gmail.com>
2022-03-05 16:07:35 +0100

    Add comment about PHP_INI_SCAN_DIR & PHP Bootstrapping argument

--- runtime/common/fpm/bootstrap.sh
+++ runtime/common/fpm/bootstrap.sh
@@ -3,6 +3,13 @@
 # Fail on error
 set -e
 
+# Every bootstrap file will be executing src/Toolbox/bootstrap.php and passing an argument
+# of which type of bootstrapping we're doing. Possible values are function|fpm|console.
+
+# We don't compile PHP anymore, so the only way to configure where PHP should be looking for
+# .ini files is via the PHP_INI_SCAN_DIR environment variable.
 export PHP_INI_SCAN_DIR="/opt/php-ini/:/var/task/php/conf.d/"
 
+# We redirect stderr to stdout so that everything
+# written on the output ends up in Cloudwatch automatically
 /opt/bin/php "/opt/bref-internal-src/vendor/bref/bref/src/Toolbox/bootstrap.php" "fpm" 2>&1
--- runtime/common/function/bootstrap.sh
+++ runtime/common/function/bootstrap.sh
@@ -3,6 +3,11 @@
 # Fail on error
 set -e
 
+# Every bootstrap file will be executing src/Toolbox/bootstrap.php and passing an argument
+# of which type of bootstrapping we're doing. Possible values are function|fpm|console.
+
+# We don't compile PHP anymore, so the only way to configure where PHP should be looking for
+# .ini files is via the PHP_INI_SCAN_DIR environment variable.
 export PHP_INI_SCAN_DIR="/opt/php-ini/:/var/task/php/conf.d/"
 
 while true
