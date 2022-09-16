54509683f8335d13bf5d41d04c127b532f84f6fc
Martincic <tomas.martincic@q.agency>
2022-09-16 16:23:13 +0200

    Switched warnings into questions

--- src/Console/Command/Init.php
+++ src/Console/Command/Init.php
@@ -5,6 +5,7 @@ namespace Bref\Console\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\ExecutableFinder;
@@ -20,6 +21,11 @@ final class Init extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
+        $helper = $this->getHelper('question');
+
+        /**
+         * @param $filesToGitAdd<string>
+         */
         $filesToGitAdd = [];
         $exeFinder = new ExecutableFinder;
         if (! $exeFinder->find('serverless')) {
@@ -45,42 +51,51 @@ final class Init extends Command
         ][$choice];
 
         $fs = new Filesystem;
-        $rootPath = dirname(__DIR__, 3) . "/template/$templateDirectory";
 
-        $io->writeln('Creating index.php');
-        if (file_exists('index.php')) {
-            $io->warning('The directory already contains a `index.php` file. Skipping...');
-        }
-        else {
-            $fs->copy("$rootPath/index.php", 'index.php');
-            $filesToGitAdd[] = 'index.php';
-        }
+        $createFile = static function ($file) use ($templateDirectory, $io, $helper, $input, $output, &$filesToGitAdd): void {
+            $write = true;
+            $rootPath = dirname(__DIR__, 3) . "/template/$templateDirectory";
+            $io->writeln("Creating $file");
+            if (file_exists($file)) {
+                $question = new ConfirmationQuestion(
+                    "An $file file already exists, do you want to overwrite it? [Y/n]",
+                    false,
+                    '/^(y|j)/i'
+                );
 
-        $io->writeln('Creating serverless.yml');
-        if (file_exists('serverless.yml')) {
-            $io->warning('The directory already contains a `serverless.yml` file. Skipping...');
-        }
-        else {
-            $template = file_get_contents("$rootPath/serverless.yml");
-            $template = str_replace('PHP_VERSION', PHP_MAJOR_VERSION . PHP_MINOR_VERSION, $template);
-            file_put_contents('serverless.yml', $template);
-            $filesToGitAdd[] = 'serverless.yml';
-        }
+                $write = $helper->ask($input, $output, $question);
+            }
+
+            if ($write) {
+                $template = file_get_contents("$rootPath/$file");
+                $template = str_replace('PHP_VERSION', PHP_MAJOR_VERSION . PHP_MINOR_VERSION, $template);
+                file_put_contents($file, $template);
+                $filesToGitAdd[] = $file;
+            }
+        };
+
+        $createFile('index.php');
+        $createFile('serverless.yml');
 
         /*
          * We check if this is a git repository to automatically add files to git.
          */
-        if ((new Process(['git', 'rev-parse', '--is-inside-work-tree']))->run() === 0) {
+        if ((new Process(['git', 'rev-parse', '--is-inside-work-tree']))->run() === 0 && $filesToGitAdd) {
+            $files = implode(',', $filesToGitAdd);
             foreach ($filesToGitAdd as $file) {
                 (new Process(['git', 'add', $file]))->run();
             }
-            $io->success([
-                'Project initialized and ready to test or deploy.',
-                'The files created were automatically added to git.',
-            ]);
-        } else {
-            $io->success('Project initialized and ready to test or deploy.');
+
+            if ($filesToGitAdd !== []) {
+                $io->success([
+                    'Project initialized and ready to test or deploy.',
+                    "The file/s $files created automatically added to git.",
+                ]);
+            }
+            return 0;
         }
+
+        $io->success('Project initialized and ready to test or deploy.');
 
         return 0;
     }
