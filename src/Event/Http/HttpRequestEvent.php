d7d4eaa66a8e7df1ee6cbce2b5bc72fc02dc366a
Nyholm <tobias.nyholm@gmail.com>
2023-01-27 14:37:04 -0800

    Removed licence text

--- src/Event/Http/HttpRequestEvent.php
+++ src/Event/Http/HttpRequestEvent.php
@@ -340,27 +340,6 @@ final class HttpRequestEvent implements LambdaEvent
      *
      * This method is heavily inspired from https://github.com/crwlrsoft/url.
      *
-     * Copyright (c) 2023 Christian Olear
-     *
-     * Permission is hereby granted, free of charge, to any person obtaining
-     * a copy of this software and associated documentation files (the
-     * "Software"), to deal in the Software without restriction, including
-     * without limitation the rights to use, copy, modify, merge, publish,
-     * distribute, sublicense, and/or sell copies of the Software, and to
-     * permit persons to whom the Software is furnished to do so, subject
-     * to the following conditions:
-     *
-     * The above copyright notice and this permission notice shall be
-     * included in all copies or substantial portions of the Software.
-     *
-     * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-     * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-     * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-     * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-     * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-     * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-     * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-     *
      * @return array<string, string>
      */
     private function queryStringToArray(string $query): array
