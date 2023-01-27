d9058583e9b065a159b877ce5e31fca4cd0c175a
Nyholm <tobias.nyholm@gmail.com>
2023-01-27 14:06:08 -0800

    cs

--- tests/Event/Http/HttpRequestEventTest.php
+++ tests/Event/Http/HttpRequestEventTest.php
@@ -166,40 +166,68 @@ class HttpRequestEventTest extends CommonHttpTest
         $this->assertEquals($expectedOutput, $result);
     }
 
-    public function queryStringProvider()
+    public function queryStringProvider(): iterable
     {
         yield ['', []];
-        yield ['foo_bar=2',  [
-            'foo_bar' => '2',
-        ]];
-        yield ['foo_bar=v1&foo.bar=v2',  [
-            'foo_bar' => 'v1',
-            'foo.bar' => 'v2',
-        ]];
-        yield ['foo_bar=v1&foo.bar=v2&foo.bar_extra=v3&foo_bar3=v4',  [
-            'foo_bar' => 'v1',
-            'foo.bar' => 'v2',
-            'foo.bar_extra' => 'v3',
-            'foo_bar3' => 'v4',
-        ]];
-       yield ['foo_bar.baz=v1',  [
-            'foo_bar.baz' => 'v1',
-        ]];
-        yield ['foo_bar=v1&k[foo.bar]=v2',  [
-            'foo_bar' => 'v1',
-            'k' => ['foo.bar' => 'v2'],
-        ]];
-        yield ['k.1=v.1&k.2[s.k1]=v.2&k.2[s.k2]=v.3',  [
-            'k.1' => 'v.1',
-            'k.2' => [
-                's.k1' => 'v.2',
-                's.k2' => 'v.3',
-            ]
-        ]];
-        yield ['foo.bar%5B0%5D=v1&foo.bar_extra%5B0%5D=v2&foo.bar.extra%5B0%5D=v3',  [
-            'foo.bar' => ['v1'],
-            'foo.bar_extra' => ['v2'],
-            'foo.bar.extra' => ['v3'],
-        ]];
+
+        yield [
+            'foo_bar=2',
+            [
+                'foo_bar' => '2',
+            ],
+        ];
+
+        yield [
+            'foo_bar=v1&foo.bar=v2',
+            [
+                'foo_bar' => 'v1',
+                'foo.bar' => 'v2',
+            ],
+        ];
+
+        yield [
+            'foo_bar=v1&foo.bar=v2&foo.bar_extra=v3&foo_bar3=v4',
+            [
+                'foo_bar' => 'v1',
+                'foo.bar' => 'v2',
+                'foo.bar_extra' => 'v3',
+                'foo_bar3' => 'v4',
+            ],
+        ];
+
+        yield [
+            'foo_bar.baz=v1',
+            [
+                'foo_bar.baz' => 'v1',
+            ],
+        ];
+
+        yield [
+            'foo_bar=v1&k[foo.bar]=v2',
+            [
+                'foo_bar' => 'v1',
+                'k' => ['foo.bar' => 'v2'],
+            ],
+        ];
+
+        yield [
+            'k.1=v.1&k.2[s.k1]=v.2&k.2[s.k2]=v.3',
+            [
+                'k.1' => 'v.1',
+                'k.2' => [
+                    's.k1' => 'v.2',
+                    's.k2' => 'v.3',
+                ],
+            ],
+        ];
+
+        yield [
+            'foo.bar%5B0%5D=v1&foo.bar_extra%5B0%5D=v2&foo.bar.extra%5B0%5D=v3',
+            [
+                'foo.bar' => ['v1'],
+                'foo.bar_extra' => ['v2'],
+                'foo.bar.extra' => ['v3'],
+            ],
+        ];
     }
 }
