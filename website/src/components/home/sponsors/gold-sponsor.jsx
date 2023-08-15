b83b5f2846c6d96013d5c9c06e47f10396c48b5f
Matthieu Napoli <matthieu@mnapoli.fr>
2023-08-15 15:48:25 +0200

    Add sponsors

---
+++ website/src/components/home/sponsors/gold-sponsor.jsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+
+export default function GoldSponsor({ src, alt, href, imgClass }) {
+    return (
+        <a className="bg-gray-400/5 p-4 sm:p-6 flex justify-center items-center"
+           href={href} title={alt}>
+            <Image
+                className={imgClass + " max-h-12 max-w-[10rem] w-full object-contain"}
+                src={src}
+                alt={alt}
+            />
+        </a>
+    );
+}
