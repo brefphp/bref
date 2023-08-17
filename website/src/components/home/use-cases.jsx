0d94cd04fcd04747df080a9ebc30c417095116d6
Matthieu Napoli <matthieu@mnapoli.fr>
2023-08-17 15:45:14 +0200

    Website: use-cases

--- website/src/components/home/use-cases.jsx
+++ website/src/components/home/use-cases.jsx
@@ -3,35 +3,35 @@ import { CheckIcon } from '@heroicons/react/20/solid';
 const features = [
     {
         name: 'Websites',
-        description: 'Run PHP websites with your favorite framework (or none), with a CDN and your custom domain.'
+        description: 'Run PHP websites with your favorite framework (or none), with a worldwide CDN and your custom domain.'
     },
     {
         name: 'HTTP APIs',
-        description: 'Corporis asperiores ea nulla temporibus asperiores non tempore assumenda aut.'
+        description: 'REST or GraphQL APIs deployed in seconds. Need more performance? Enable <strong>Laravel Octane</strong> or the Symfony equivalent.'
     },
     {
         name: 'CLI commands',
-        description: 'In sit qui aliquid deleniti et. Ad nobis sunt omnis. Quo sapiente dicta laboriosam.'
+        description: 'Run DB migrations, admin commands, or any other CLI command from your machine or your CI/CD.'
     },
     {
         name: 'Cron tasks',
-        description: 'Sed rerum sunt dignissimos ullam. Iusto iure occaecati voluptate eligendi fugiat sequi.'
+        description: 'Every day, every hour, every minute… Yes, The Cloud™ can do that.'
     },
     {
         name: 'Job queues',
-        description: 'Quos inventore harum enim nesciunt. Aut repellat rerum omnis adipisci.'
+        description: 'Run 1000 jobs with 1 worker in 1000 seconds, or with <strong>1000 workers</strong> in 1 second. It\'s just as simple and it costs the same. SQS provides batching, retries, and failure handling.'
     },
     {
         name: 'Event-driven microservices',
-        description: 'Eos laudantium repellat sed architecto earum unde incidunt. Illum sit dolores voluptatem.'
+        description: 'Decouple and scale microservices without container madness. Send messages to EventBridge and let it invoke your PHP classes directly. No integration to write.'
     },
     {
         name: 'File processing',
-        description: 'Nulla est saepe accusamus nostrum est est. Fugit voluptatum omnis quidem voluptatem.'
+        description: 'S3 can invoke a PHP class whenever a new file is uploaded. Resize images, convert videos, generate PDFs…'
     },
     {
         name: 'WebSockets',
-        description: 'Nulla est saepe accusamus nostrum est est. Fugit voluptatum omnis quidem voluptatem.'
+        description: 'AWS API Gateway manages the WebSocket connections for you. Send messages to your users in real-time.'
     },
 ];
 
@@ -44,9 +44,18 @@ export default function UseCases() {
                     <div>
                         <h2 className="mt-2 text-3xl font-black tracking-tight text-gray-900 sm:text-4xl">Use cases</h2>
                         <p className="mt-6 text-base leading-7 text-gray-600">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis
-                            suscipit eaque, iste
-                            dolor cupiditate blanditiis ratione.
+                            Serverless means whatever you choose it to mean.
+                        </p>
+                        <p className="mt-6 text-base leading-7 text-gray-600">
+                            Run PHP as usual, {' '}<strong>like on any server</strong>.
+                            Except it scales (almost) infinitely and you don't maintain the infrastructure.<br/>
+                            Lift-and-shift existing apps or build new ones with your favorite framework.
+                        </p>
+                        <p className="mt-6 text-base leading-7 text-gray-600">
+                            Or go the extreme opposite: build cloud-native {' '}<strong>event-driven microservices</strong> with infinitely scalable cloud services like SQS and EventBridge.
+                        </p>
+                        <p className="mt-6 text-base leading-7 text-gray-600">
+                            Or anything in between, that works too.
                         </p>
                     </div>
                     <dl className="col-span-2 grid grid-cols-1 gap-x-8 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:gap-y-16">
@@ -57,7 +66,7 @@ export default function UseCases() {
                                                aria-hidden="true" />
                                     {feature.name}
                                 </dt>
-                                <dd className="mt-2">{feature.description}</dd>
+                                <dd className="mt-2" dangerouslySetInnerHTML={{ __html: feature.description }}></dd>
                             </div>
                         ))}
                     </dl>
