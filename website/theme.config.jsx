62141a54b7c0ff6caad1b0b50852576b37829071
Matthieu Napoli <matthieu@mnapoli.fr>
2023-10-11 17:27:55 +0200

    Website: fix SSG

--- website/theme.config.jsx
+++ website/theme.config.jsx
@@ -12,6 +12,7 @@ export default {
     },
     useNextSeoProps() {
         const { asPath } = useRouter();
+        console.log(asPath);
         // On SSG the path is `/index` instead of `/` for some reason
         if (asPath === '/' || asPath === '/index') {
             return {
