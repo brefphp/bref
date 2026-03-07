07a9d1018790902ec2848d0ea5b0a499bd3742ee
Matthieu Napoli <matthieu@mnapoli.fr>
2026-03-07 13:53:37 +0100

    Switch to Mailcoach

--- website/src/components/home/case-studies.jsx
+++ website/src/components/home/case-studies.jsx
@@ -38,23 +38,16 @@ export default function CaseStudies() {
                                 Support & consulting <span aria-hidden="true">&rarr;</span>
                             </a>
                         </div>
-                        <form action="https://app.convertkit.com/forms/5696241/subscriptions"
+                        <form action="https://bref.mailcoach.app/subscribe/be83c960-8fee-43fa-abfa-29669e9f433a"
                               className="w-full flex flex-col items-start mt-8"
-                              method="post" data-sv-form="5696241" data-uid="ee838f35c4" data-format="inline" data-version="5"
-                              data-options="{&quot;settings&quot;:{&quot;after_subscribe&quot;:{&quot;action&quot;:&quot;message&quot;,&quot;success_message&quot;:&quot;Success! Now check your email to confirm your subscription.&quot;,&quot;redirect_url&quot;:&quot;&quot;},&quot;analytics&quot;:{&quot;google&quot;:null,&quot;fathom&quot;:null,&quot;facebook&quot;:null,&quot;segment&quot;:null,&quot;pinterest&quot;:null,&quot;sparkloop&quot;:null,&quot;googletagmanager&quot;:null},&quot;modal&quot;:{&quot;trigger&quot;:&quot;timer&quot;,&quot;scroll_percentage&quot;:null,&quot;timer&quot;:5,&quot;devices&quot;:&quot;all&quot;,&quot;show_once_every&quot;:15},&quot;powered_by&quot;:{&quot;show&quot;:true,&quot;url&quot;:&quot;https://convertkit.com/features/forms?utm_campaign=poweredby&amp;utm_content=form&amp;utm_medium=referral&amp;utm_source=dynamic&quot;},&quot;recaptcha&quot;:{&quot;enabled&quot;:false},&quot;return_visitor&quot;:{&quot;action&quot;:&quot;show&quot;,&quot;custom_content&quot;:&quot;&quot;},&quot;slide_in&quot;:{&quot;display_in&quot;:&quot;bottom_right&quot;,&quot;trigger&quot;:&quot;timer&quot;,&quot;scroll_percentage&quot;:null,&quot;timer&quot;:5,&quot;devices&quot;:&quot;all&quot;,&quot;show_once_every&quot;:15},&quot;sticky_bar&quot;:{&quot;display_in&quot;:&quot;top&quot;,&quot;trigger&quot;:&quot;timer&quot;,&quot;scroll_percentage&quot;:null,&quot;timer&quot;:5,&quot;devices&quot;:&quot;all&quot;,&quot;show_once_every&quot;:15}},&quot;version&quot;:&quot;5&quot;}">
-                            <ul className="text-red-400 font-bold" data-element="errors" data-group="alert"></ul>
-                            <div data-element="fields" data-stacked="false"
-                                 className="flex flex-col sm:flex-row w-full sm:w-auto gap-2">
+                              method="post">
+                            <input type="hidden" name="tags" value="bref_website" />
+                            <div className="flex flex-col sm:flex-row w-full sm:w-auto gap-2">
                                 <input
                                     className="min-w-0 flex-auto rounded-md border-0 bg-white/10 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-white/75 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
-                                    name="email_address" aria-label="Email" placeholder="you@example.com" required type="email" />
-                                <button data-element="submit" type="submit"
+                                    name="email" aria-label="Email" placeholder="you@example.com" required type="email" />
+                                <button type="submit"
                                         className="flex-none rounded-md !bg-gray-500 hover:!bg-gray-400 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500">
-                                    <div className="formkit-spinner">
-                                        <div></div>
-                                        <div></div>
-                                        <div></div>
-                                    </div>
                                     Subscribe to the newsletter
                                 </button>
                             </div>
