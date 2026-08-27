df7746800b42f6888aa38d02a89eb30077ccf5e7
Matthieu Napoli <matthieu@mnapoli.fr>
2026-08-27 23:03:12 +0200

    Publish August 2026 newsletter issue

--- website/content/news/_meta.js
+++ website/content/news/_meta.js
@@ -1,4 +1,5 @@
 export default {
+    '04-august-2026': 'August 2026: osls v4 and zero-downtime assets',
     '03-bref-3.0': 'Bref 3.0 is released',
     '02-bref-2.0': 'Bref 2.0 is released',
     '01-bref-1.0': 'Bref 1.0 is released',
