578bc9b519f8927f71fc52d28e1cd5091310df3e
Matthieu Napoli <matthieu@mnapoli.fr>
2026-09-22 22:14:04 +0100

    Documentation: Migrating from Vapor

--- docs/laravel/_meta.js
+++ docs/laravel/_meta.js
@@ -1,8 +1,9 @@
 export default {
-  "getting-started": "Getting started",
-  "environments": "Environments",
-  "file-storage": "",
-  "queues": "Laravel Queues",
-  "octane": "Laravel Octane",
-  "passport": "Laravel Passport"
-}
+    'getting-started': 'Getting started',
+    'migrating-from-vapor': 'Migrating from Vapor',
+    environments: 'Environments',
+    'file-storage': '',
+    queues: 'Laravel Queues',
+    octane: 'Laravel Octane',
+    passport: 'Laravel Passport',
+};
